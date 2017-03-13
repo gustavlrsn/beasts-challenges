774847cbc74ba57a6fa9e2176db60d25460f304e
Gustav Larsson <gustav.larsson@gmail.com>
2017-03-13 18:49:24 +0100

    Updated isPrototypeOf
    Now covers all cases

--- isPrototypeOf/isPrototypeOf.js
+++ isPrototypeOf/isPrototypeOf.js
@@ -1,10 +1,16 @@
 // Challenge: Write a function, isPrototypeOf, that works just like Object.prototype.isPrototypeOf
 
 function isPrototypeOf(prototype, object) {
+  // Base  case 0:
+  if(prototype === null || prototype === undefined) {
+    throw new TypeError;
   // Base case 1:
-  if(Object.getPrototypeOf(object) === prototype) {
+  } else if (Object.getPrototypeOf(object) === prototype) {
     return true;
   // Base case 2:
+  } else if (Object.getPrototypeOf(object) === null) {
+    return false;
+  // Base case 3:
   } else if (object === Object.prototype) {
     return false;
   // Recursion:
